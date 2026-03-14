721e123c532da47ed72e68a3a3260897aab7be27
Jack Kaloger <jkaloger@users.noreply.github.com>
2026-03-14 17:03:57 +1100

    chore: add glow, rg, zoxide, shellcheck

--- nix/modules/shared/packages.nix
+++ nix/modules/shared/packages.nix
@@ -8,13 +8,18 @@ with pkgs;
   deno
   fzf
   gh
+  glow
   gum
   neovim
   nixfmt-rfc-style
+  ripgrep
   rust-analyzer
   rustc
   rustfmt
   sesh
+  shellcheck
+  stylua
   slides
   stow
+  zoxide
 ]
