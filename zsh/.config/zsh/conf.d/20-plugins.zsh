c1ab15e6afe462fb063aa4d23115e971dafbef4b
Jack Kaloger <jkaloger@users.noreply.github.com>
2026-03-25 23:12:02 +1100

    fix linux issues

--- zsh/.config/zsh/conf.d/20-plugins.zsh
+++ zsh/.config/zsh/conf.d/20-plugins.zsh
@@ -3,4 +3,8 @@ source "$XDG_CONFIG_HOME/zsh/antigen.zsh"
 antigen bundle zsh-users/zsh-autosuggestions
 antigen apply
 
-source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+if [[ -f /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]]; then
+  source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+elif [[ -f /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]]; then
+  source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+fi
--- zsh/.config/zsh/conf.d/30-tools.zsh
+++ zsh/.config/zsh/conf.d/30-tools.zsh
@@ -1,4 +1,6 @@
-eval "$(fnm env --use-on-cd --shell zsh)"
+if command -v fnm &>/dev/null; then
+  eval "$(fnm env --use-on-cd --shell zsh)"
+fi
 eval "$(starship init zsh)"
 eval "$(zoxide init zsh)"
 eval "$(direnv hook zsh)"
